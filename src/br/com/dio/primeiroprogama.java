246f38eb4a037ea26f0a372b61b945965c9768ef
Paulo Vinicius BR <paulo_vinicius2012@hotmail.com.br>
2022-03-14 22:37:07 -0300

    firt commit

---
+++ src/br/com/dio/primeiroprogama.java
@@ -0,0 +1,22 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class primeiroprogama {
+    public static void main(String[] args) {
+
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+
+
+       /*int a = 5;
+       int b = 3;
+       System.out.println("Heloo World " + (a+b));*/
+
+    }
+
+
+}
