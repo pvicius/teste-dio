fec769e4b1c4e7be473ca4f8056c031a63d12fbe
Paulo Vinicius BR <paulo_vinicius2012@hotmail.com.br>
2022-03-14 23:31:04 -0300

    criando class Livro

--- src/br/com/dio/primeiroprogama.java
+++ src/br/com/dio/primeiroprogama.java
@@ -3,12 +3,17 @@ package br.com.dio;
 import br.com.dio.model.Gato;
 
 public class primeiroprogama {
-    public static void main(String[] args) {
+    public static  void main(String[] args) {
 
 
         Gato gato = new Gato();
 
         System.out.println(gato);
+
+
+        String nome;
+        Livro livro1 = new Livro("Pense De Novo",  302);
+     System.out.println(livro1);
 
 
 
@@ -20,3 +25,40 @@ public class primeiroprogama {
 
 
 }
+
+class Livro {
+
+    private String nome;
+    private Integer numPaginas;
+
+    public Livro(String nome, Integer numPaginas) {
+        this.nome = nome;
+        this.numPaginas = numPaginas;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPaginas() {
+        return numPaginas;
+    }
+
+    public void setNumPaginas(Integer numPaginas) {
+        this.numPaginas = numPaginas;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPaginas=" + numPaginas +
+                '}';
+    }
+}
+
